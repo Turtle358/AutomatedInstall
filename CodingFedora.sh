1fe8db3a02665605da1260860371d09f30d58a91
Turtle358 <118539597+Turtle358@users.noreply.github.com>
2023-08-14 19:10:12 +0100

    linuxsdafghtjk

---
+++ CodingFedora.sh
@@ -0,0 +1,4 @@
+sudo -i
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.jetbrains.IntelliJ-IDEA-Ultimate com.jetbrains.PyCharm-Professional com.jetbrains.WebStorm com.jetbrains.Rider com.google.AndroidStudio com.discordapp.Discord com.spotify.Client md.obsidian.Obsidian
+dnf install gnome-tweaks 
