094e9adc20d224e45534775275d731d5eb0be8cb
Turtle358 <118539597+Turtle358@users.noreply.github.com>
2023-08-15 10:51:59 +0100

    Update WindowsGaming.bat

--- WindowsGaming.bat
+++ WindowsGaming.bat
@@ -11,6 +11,7 @@ winget install jetbrains.pycharm.professional
 winget install git.git 
 winget install Chocolatey.Chocolatey 
 winget install Oracle.VirtualBox
+winget install Python.Python.3.11
 echo Anihilating Edge
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\EdgeUpdate" /v "DoNotUpdateToEdgeWithChromium" /d "0x0001" /f >nul 2>&1
 taskkill /f /im Widgets.exe
