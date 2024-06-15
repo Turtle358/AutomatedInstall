2425a23e01ce19e3e16195b5ffd709824e13ea61
Turtle358 <118539597+Turtle358@users.noreply.github.com>
2024-06-15 19:06:34 +0100

    Update CodingFedora.sh

--- CodingFedora.sh
+++ CodingFedora.sh
@@ -1,4 +1,4 @@
 sudo -i
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.jetbrains.IntelliJ-IDEA-Ultimate com.jetbrains.PyCharm-Professional com.jetbrains.WebStorm com.jetbrains.Rider com.google.AndroidStudio com.discordapp.Discord com.spotify.Client md.obsidian.Obsidian
+flatpak install flathub com.jetbrains.PyCharm-Professional com.discordapp.Discord com.spotify.Client md.obsidian.Obsidian
 dnf install gnome-tweaks 
